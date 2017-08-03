6cd518ca26bcd8a77fa91cb950f48cd7ec43d98e
Alexander Levchenko <alexander.levchenko@nixsolutions.com>
2017-08-03 18:57:50 +0300

    Done

---
+++ src/reducers/reducer-books.js
@@ -0,0 +1,1 @@
+export default function () {  return [    { title: 'Javascript: The Good Parts', pages: 101 },    { title: 'Harry Potter', pages: 39 },    { title: 'The Dark Tower', pages: 85 },    { title: 'Eloguent Ruby', pages: 1 },  ]}
